cb0681f63cc88ec7ec2261641fe35f41c8865f43
InimitablePioneer <seungwonpioneer@gmail.com>
2024-08-19 15:08:23 +0900

    AUG19 at work via WIN

---
+++ src/main/java/hello/typeconverter/controller/ConverterController.java
@@ -0,0 +1,30 @@
+package hello.typeconverter.controller;
+
+import hello.typeconverter.type.IpPort;
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class ConverterController {
+
+    @GetMapping("/converter-view")
+    public String converterView(Model model) {
+        model.addAttribute("number", 10000);
+        model.addAttribute("ipPort", new IpPort("127.0.0.1", 8080));
+        return "converter-view";
+    }
+
+
+    @Data
+    static class Form{
+        private IpPort ipPort;
+
+        public Form(IpPort ipPort) {
+            this.ipPort = ipPort;
+        }
+    }
+
+}
